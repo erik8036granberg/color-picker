78121d30077756b7dc2f19fe3f481aaccb260115
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-02-13 08:27:26 +0100

    RGB color convertion OK

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 
 let colorWell;
 const defaultColor = "#0000ff";
-let colorHarmony = "monochromatic";
-let hexColor;
+
+// let colorHarmony = "monochromatic";
+// let convertRgbColor;
 
 const box_1 = document.querySelector("#box_1");
 const box_2 = document.querySelector("#box_2");
@@ -14,40 +15,55 @@ const box_5 = document.querySelector("#box_5");
 document.addEventListener("DOMContentLoaded", init, false);
 
 function init() {
-    console.log("init");
-    colorWell = document.querySelector("#colorWell");
-    colorWell.value = defaultColor;
-    colorWell.addEventListener("input", colorPick, false);
-    colorWell.select();
+  console.log("init");
+  colorWell = document.querySelector("#colorWell");
+  colorWell.value = defaultColor;
+  colorWell.addEventListener("input", colorPick, false);
+  colorWell.select();
 }
 
 function colorPick(event) {
-    console.log("colorPick");
+  console.log("colorPick");
 
-    // Get color from picker
-    hexColor = event.target.value;
-    console.log("hexColor");
+  // Get hex color from picker
+  let hexColor = event.target.value;
+  console.log("hexcolor is: " + hexColor);
 
-    setBaseColor();
+  // hex to rgb
+  let rgbColor = hexToRgb(hexColor);
+  console.log("rgbColor is: " + rgbColor);
+
+  // setBaseColor();
+  setColor(rgbColor);
 }
 
 function setBaseColor() {
-    console.log("setBaseColor");
+  console.log("setBaseColor");
 
-    calculateColors();
+  calculateColors();
 }
 
 function calculateColors() {
-    console.log("calculateColors");
+  console.log("calculateColors");
 
-    setColor();
+  setColor();
 }
 
-function setColor() {
-    console.log("setColor");
-    box_1.style.backgroundColor = hexColor;
+function setColor(color) {
+  console.log("setColor");
+
+  // set box_1 background color
+  box_1.style.backgroundColor = color;
 }
 
-function hexToRgb() {
-
+function hexToRgb(hex) {
+  let x = [];
+  hex = hex.replace("#", "");
+  if (hex.length != 6) {
+    hex = modifyHex(hex);
+  }
+  x.push(parseInt(hex.slice(0, 2), 16));
+  x.push(parseInt(hex.slice(2, 4), 16));
+  x.push(parseInt(hex.slice(4, 6), 16));
+  return "rgb(" + x.toString() + ")";
 }
